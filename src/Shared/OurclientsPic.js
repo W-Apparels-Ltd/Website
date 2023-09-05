b416b7410a0501ecb9c2d77b35fb0be54a80c714
Tonmoy Debnath <tonmoy.debnath@northsouth.edu>
2023-09-05 10:22:25 +0600

    hover added to our client

--- src/Shared/OurclientsPic.js
+++ src/Shared/OurclientsPic.js
@@ -3,7 +3,7 @@ import React from 'react';
 const OurclientsPic = ({item}) => {
   return (
     <div className='flex justify-center'>
-      <img className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover: duration-300' src={item.img} alt="" />
+      <img className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-150 hover: duration-300' src={item.img} alt="" />
     </div>
   );
 };
