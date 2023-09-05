19d800afe2924d156604ce43762390de1fc96a39
Tonmoy Debnath <tonmoy.debnath@northsouth.edu>
2023-09-05 10:32:28 +0600

    career psoition fixed

--- src/pages/CareerForm.js
+++ src/pages/CareerForm.js
@@ -206,41 +206,7 @@ const CareerForm = () => {
             <div>
             <form onSubmit={handleSubmit}>
       <div className="space-y-12">
-        <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            This information will be displayed publicly so be careful what you share.
-          </p>
-          
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-           
-
-            <div className="col-span-full">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                Cover Letter
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="coverletter"
-                  name="coverletter"
-                  rows={3}
-                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                />
-              </div>
-              <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-            </div>
-
-
-            <div className="col-span-full">
-              <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                Upload your CV
-              </label>
-                <input type="file" name='cv' class="mt-5 file-input file-input-primary file-input-bordered w-full max-w-xs" />
-
-            </div>
-          </div>
-        </div>
+      <div className="border-b border-gray-900/10 pb-12"></div>
 
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
@@ -371,6 +337,38 @@ const CareerForm = () => {
         </div>
 
       </div>
+      <div className="border-b border-gray-900/10 pb-12">
+          
+          
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+           
+
+            <div className="col-span-full">
+              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                Cover Letter
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="coverletter"
+                  name="coverletter"
+                  rows={3}
+                  className="block w-full rounded-md border-0 p-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={''}
+                />
+              </div>
+              <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
+            </div>
+
+
+            <div className="col-span-full">
+              <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
+                Upload your CV
+              </label>
+                <input type="file" name='cv' class="mt-5 file-input file-input-primary file-input-bordered w-full max-w-xs" />
+
+            </div>
+          </div>
+        </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
         <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
