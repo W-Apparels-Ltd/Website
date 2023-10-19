04dfe5f639faa29aec0409cafa870074e72145bc
Tonmoy Debnath <tonmoy.debnath@northsouth.edu>
2023-10-19 16:54:56 +0600

    meet the team button fixed

--- src/Shared/CorporateViewpoint.js
+++ src/Shared/CorporateViewpoint.js
@@ -2,6 +2,7 @@ import React from 'react';
 import img from '../images/factory_purpose/333333.jpg'
 import grp from '../images/personnel/GroupPic.jpg'
 import arrow from '../images/Untitled__1_-removebg-preview.png'
+import { Link } from 'react-router-dom';
 
 const CorporateViewpoint = () => {
   return (
@@ -16,11 +17,13 @@ const CorporateViewpoint = () => {
                 <p className="pt-6 lg:px-20 px-5 text-white lg:text-xl text-md">
                 W. Apparels Ltd is among one of the most successful garment manufacturers in Bangladesh; a country with great potential in the field of garment-manufacturing. It started its journey in the year 1992. W. Apparels Ltd has passed over 17 successful years of accomplishment, aiming to lead the competitive knit/Woven/Non-Woven wear-organizations of Bangladesh. It has arduously reengineered each step of its value-chain to match the dynamism of the fashion market. W. Apparels Ltd has expanded its capacity, developed a skilled workforce and introduced integrated technologies, to remain globally up-to-date. It is truly an organization with a glorious past and a bright future.</p>  
                 <div className='flex flex-row-reverse'> 
+                  <Link to='/meettheteam'>
                   <button className='btn btn-ghost text-white font-bold mt-20 lg:me-5 md:me-5'>Meet Our Team
                   <span className=" w-12 transition-colors group-hover:bg-[#000066]">
                   <img src={arrow}></img>
                   </span>
                   </button>  
+                  </Link>
                 </div>
       </div>
       
