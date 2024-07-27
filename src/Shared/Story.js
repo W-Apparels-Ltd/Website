f142073baed5a22527567475b45db6403a589f14
DESKTOP-TUDPHM1\Tonmoy <tonmoy.debnath@northsouth.edu>
2024-07-27 14:02:15 +0600

    our story button changed and picture saved

--- src/Shared/Story.js
+++ src/Shared/Story.js
@@ -48,14 +48,21 @@ const Story = () => {
             <div className='flex justify-end'>
           
             <Link  to={pdf} target='_blank' rel='noreferrer'
-              class="group relative flex select-none items-center gap-3 overflow-hidden rounded-lg bg-[#000066] py-3 px-7 pr-[72px] text-center align-middle text-sm font-bold uppercase text-white shadow-md shadow-light-blue-500/20 transition-all hover:shadow-lg active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-nonegroup relative flex select-none items-center gap-3 overflow-hidden rounded-lg bg-gradient-to-tr from-light-blue-900 to-sky-900 py-2 px-5 pr-[72px] text-center align-middle text-sm font-bold uppercase text-white shadow-md shadow-light-indigo-500/20 transition-all hover:shadow-lg hover:shadow-light-blue-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
-              type="button"
-              data-ripple-light="true"
+              className="group "
+              
             >
-              View Corporate Profile
-              <span className="absolute right-0 grid h-full w-12 place-items-center bg-[#00004d] transition-colors group-hover:bg-[#000066]">
-                <img src={arrow}></img>
-              </span>
+              
+              <button className='mb-12 lg:mb-0 text-white lg:ms-5 md:ms-5'>
+                  <div className='flex items-center'>
+                    <h1 className='text-md font-bold'>View Corporate Profile
+                    <div className='bg-white h-[2px] w-0 group-hover:w-full transition-all duration-500'></div>
+                    </h1>
+                  
+                    <img className='w-12' src={arrow}></img>
+                  </div>
+                  
+                 
+            </button> 
             </Link>
             
             </div>
