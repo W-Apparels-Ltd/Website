1b29146139b2f47da0429d62fb23b6ec8ac33942
Mahi Alam Azad <mahialamazads@gmail.com>
2026-08-03 20:57:26 +0600

    ccenterd a important div for certificates

--- src/Shared/OurCertificatesPic.js
+++ src/Shared/OurCertificatesPic.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const OurCertificatesPic = ({ item }) => {
   return (
-    <div className="flex items-center justify-center h-24 w-48 md:h-28 md:w-56 lg:h-32 lg:w-64">
+    <div className="flex h-[180px] items-center justify-center px-4">
       <img
         src={item.img}
         alt={item.name || ""}
-        className="max-h-full max-w-full object-contain"
+        className="block max-h-[150px] max-w-[150px] object-contain object-center"
       />
     </div>
   );
