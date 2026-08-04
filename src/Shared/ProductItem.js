71fc8c12ab873dd620dc71cf62de78cb9f6479e8
Mahi Alam Azad <mahialamazads@gmail.com>
2026-08-04 16:24:04 +0600

    trying to fix product page

--- src/Shared/ProductItem.js
+++ src/Shared/ProductItem.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const ProductItem = ({item}) => {
+const ProductItem = ({ item, product }) => {
+  const productData = item || product || {};
+  const imageList = Array.isArray(productData.img) ? productData.img : [];
+  const productImage = imageList[1] || imageList[0] || '';
+
+  if (!productData._id) {
+    return null;
+  }
+
   return (
                 <div className="card bg-base-100 shadow-xl m-5 ">
                   <figure >
-                  <img className='object-contain h-[250px] p-5' src={item.img[1]} alt="Shoes" />
+                  <img className='object-contain h-[250px] p-5' src={productImage} alt={productData.label || 'Product'} />
                   </figure>
                   <div className='m-5 text-center'>
-                    <h2 className='font-bold'>{item.label}</h2>
-                    <p className='mb-2'>Size: {item.size}</p>
+                    <h2 className='font-bold'>{productData.label}</h2>
+                    <p className='mb-2'>Size: {productData.size}</p>
                     <div>
-                      <Link to={`/${item._id}`}>
+                      <Link to={`/${productData._id}`}>
                         <button className="btn btn-primary btn-sm">Inquire Now</button>
                       </Link>
                     </div>
